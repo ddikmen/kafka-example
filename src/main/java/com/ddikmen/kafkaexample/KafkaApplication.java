dec0ab13ccdf473326bf7c2c4f77c60d97b66b73
Doğukan Dikmen <dikmenh15@gmail.com>
2022-04-12 11:48:50 +0300

    cleaned unnecessary imports

--- src/main/java/com/ddikmen/kafkaexample/KafkaApplication.java
+++ src/main/java/com/ddikmen/kafkaexample/KafkaApplication.java
@@ -1,10 +1,7 @@
 package com.ddikmen.kafkaexample;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaApplication {
